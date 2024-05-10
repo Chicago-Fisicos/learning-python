dc0474dc6c1235c11555777afe71d72651d9ba0c
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-05-10 17:47:04 -0300

    requirements

--- funciones.py
+++ funciones.py
@@ -1,15 +1,14 @@
 
 
-def sumar(a, b):
+def sumar(a=1, b=5):
     return a + b
+
 def restar(a, b):
     return a - b
 
 if __name__ == "__main__":
-    resultado = sumar(5, 3)
-    var_1 = sumar(3, 1)
-    var_2 = sumar(5, 1)
-    var_3 = restar(20,3)
-    var_4 = restar(30,5)
+
+
+    resultado = sumar()
 
     print(f"El resultado de la suma es: {resultado}")
