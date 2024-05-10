c71a921f7f9dc54f6ce7dbc63b9b3e5d59da5c38
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-05-10 17:28:45 -0300

    primer commit

---
+++ funciones.py
@@ -0,0 +1,10 @@
+
+
+def sumar(a, b):
+    return a + b
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    resultado = sumar(5, 3)
+    print(f"El resultado de la suma es: {resultado}")
---
+++ main.py
@@ -0,0 +1,11 @@
+# desde el archivo tal importo la funcion tal
+from funciones import *
+
+
+resultado1 = sumar(2,3)
+resultado2 = restar(3,1)
+
+print(resultado1)
+print(resultado2)
+print(f'La suma es {resultado1} y la resta es {resultado2}')
+
